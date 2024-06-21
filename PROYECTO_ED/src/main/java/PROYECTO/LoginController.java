252547c74acb9fea41d2ed11983e7d84b4d0b937
DERS0214 <dramirez0214@hotmail.com>
2024-06-21 17:20:27 -0500

    AP login

--- PROYECTO_ED/src/main/java/PROYECTO/LoginController.java
+++ PROYECTO_ED/src/main/java/PROYECTO/LoginController.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 
@@ -13,6 +14,27 @@ public class LoginController {
     private DoublyCircularList<String> list=new DoublyCircularList<String>();
     private DoublyCircularNode<String> currentNode;
     private Label prueba;
+    
+        @FXML
+    private Button BT2;
+
+    @FXML
+    private Button BT1;
+
+    @FXML
+    private Label PRUEBA;
+
+    @FXML
+    public void adelante() {
+        currentNode=currentNode.getNext();
+        PRUEBA.setText(currentNode.getContent());
+    }
+
+    @FXML
+    public void atras() {
+        currentNode=currentNode.getPrevious();
+        PRUEBA.setText(currentNode.getContent());
+    }
     
      @FXML
     public void initialize() {
