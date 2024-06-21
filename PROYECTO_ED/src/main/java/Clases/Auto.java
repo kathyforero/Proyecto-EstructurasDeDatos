81e8981ade95276dbbbce4490848f54411e6cca3
Katherine Forero <143484307+kathyforero@users.noreply.github.com>
2024-06-21 15:43:10 -0500

    Commit #2

--- PROYECTO_ED/src/main/java/Clases/Auto.java
+++ PROYECTO_ED/src/main/java/Clases/Auto.java
@@ -9,5 +9,28 @@ package Clases;
  * @author Kathy
  */
 public class Auto {
+    private float precio;
+    private String marca;
+    private String modelo;
+    private String tipo;
+    private int año;
+    private int kilometraje;
+    private String motor;
+    private String transmisión;
+    private float peso;
+    private String ubicacion;
+    
+    public Auto(float precio, String marca, String modelo, String tipo, int año, int kilometraje, String motor, String transmisión, float peso, String ubicacion){
+        this.precio = precio;
+        this.marca = marca;
+        this.modelo = modelo;        
+        this.tipo = tipo;
+        this.año = año;
+        this.kilometraje = kilometraje;
+        this.motor = motor;
+        this.transmisión = transmisión;        
+        this.peso = peso;
+        this.ubicacion = ubicacion;
+    }
     
 }
