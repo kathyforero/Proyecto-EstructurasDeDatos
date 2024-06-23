b50936a7c86fcd5dc8856f3d2032822918efc9d4
Bolaños Fajardo Tomás Steven <tbolanos@espol.edu.ec>
2024-06-23 13:03:18 -0500

    cambios en arraylist
    se agregaron comentarios, cuerpo en set, remove (index); y algunas excepciones como indexoutofbounds

--- PROYECTO_ED/src/main/java/Bases/ArrayList.java
+++ PROYECTO_ED/src/main/java/Bases/ArrayList.java
@@ -8,50 +8,54 @@ public class ArrayList<E> implements List<E>{
     private int capacity = 100;
     private int effectiveSize;
     
+    /**
+     * Constructor crea una lista de genéricos con una capacidad maxima de 100
+     */
     public ArrayList (){
-        
-        //int[] a;
-        
-        //a = new int[100];
-        
-        //elements = new E[100]; NO FUNCIONA
-        
-        elements = (E[])(new Object[capacity]); // SI FUNCIONA con Casting permitido con el arrayList
+        elements = (E[])(new Object[capacity]);
         effectiveSize = 0;
     }
     
+    /**
+     * Verifica si la lista está llena
+     * @return true si la lista está llena, false si no está llena
+     */
     private boolean isFull(){
         return effectiveSize == capacity;
     }
    
+    /**
+     * Agrega un elemento al principio de la lista.
+     *
+     * @param e el elemento genérico que se va a agregar
+     * @return true si se logró insertar el elemento, false si el objeto es nulo
+     */
     @Override
     public boolean addFirst(E e) {
-        
-        // no se insertan nulos
-        if(e==null){
+        if (e == null) {
             return false;
-        } else if (isEmpty()){
-            elements[0] = e;
-            effectiveSize++;
-            //elements[effectiveSize ++] = e; //otra forma de hacer las 2 lineas anteriores
-            return true;
-        } else if (isFull()){
+        }
+        if (isFull()) {
             addCapacity();
         }
-        
-        for (int i=effectiveSize-1; i >=0; i--){
-            elements[i+1]=elements[i]; //bit shifting 
-            //desplazamiento de valores hacia la derecha, 
-            //debe empezar de atras hacia adelante
+        for (int i = effectiveSize; i > 0; i--) {
+            elements[i] = elements[i - 1];
         }
         elements[0] = e;
         effectiveSize++;
         return true;
     }
 
+
+    /**
+     * Agrega un elemento al final de la lista.
+     *
+     * @param e el elemento genérico a agregar
+     * @return true si se logró insertar el elemento, false si el objeto es nulo
+     */
     @Override
     public boolean addLast(E e) {
-       if (e == null) {
+        if (e == null) {
             return false;
         }
         if (isFull()) {
@@ -59,9 +63,166 @@ public class ArrayList<E> implements List<E>{
         }
         elements[effectiveSize] = e;
         effectiveSize++;
-        //elements[effectiveSize++] = e; otra forma de hacer las 2 lineas anteriores
         return true;
     }
+
+
+    /**
+     * Devuelve la longitud usada de la lista
+     * @return effectiveSize, que es la longitud usada de la lista 
+     */
+    @Override
+    public int size() {
+        return effectiveSize;
+    }
+
+    /**
+     * Verifica si la lista está vacía
+     * @return true si la lista está vacía, false si no está vacía
+     */
+    @Override
+    public boolean isEmpty() {
+        return effectiveSize == 0;
+    }
+
+    /**
+     * Agrega un elemento en una posición dentro de la lista.
+     *
+     * @param index la posición en la que se agregará el elemento
+     * @param element el elemento genérico que se va a agregar en la lista
+     * @throws IndexOutOfBoundsException si el índice está fuera del rango de la lista
+     */
+    @Override
+    public void add(int index, E element) {
+        if (index < 0 || index > effectiveSize) {
+            throw new IndexOutOfBoundsException("No se pudo agregar el objeto en la posición: " + index);
+        }
+        if (isFull()) {
+            addCapacity();
+        }
+        for (int i = effectiveSize; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = element;
+        effectiveSize++;
+    }
+
+    /**
+     * Duplica la capacidad del arreglo de elementos.
+     * Este método se llama cuando la lista está llena y necesita más espacio.
+     */
+    private void addCapacity() {
+        E[] tmp = (E[]) new Object[capacity * 2];
+        for (int i = 0; i < capacity; i++) {
+            tmp[i] = elements[i];
+        }
+        elements = tmp;
+        capacity = capacity * 2;
+    }
+
+    /**
+     * Devuelve una String que contiene concatenados los elementos de la lista.
+     *
+     * @return una cadena que representa los elementos de la lista 
+     */
+    @Override
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < effectiveSize; i++) {
+            s += elements[i] + ", ";
+        }
+        return s;
+    }
+
+    /**
+     * Devuelve un iterador sobre los elementos de la lista en el orden correcto.
+     *
+     * @return un iterador sobre los elementos de la lista
+     */
+    @Override
+    public Iterator<E> iterator() {
+        Iterator<E> it = new Iterator<E>() {
+            int cursor = 0;
+
+            /**
+             * Verifica si hay más elementos en la lista.
+             *
+             * @return true si hay más elementos, false si la lista ya no tiene más elementos
+             */
+            @Override
+            public boolean hasNext() {
+                return cursor < effectiveSize;
+            }
+
+            /**
+             * Devuelve el siguiente elemento en la lista.
+             *
+             * @return el siguiente elemento en la lista
+             */
+            @Override
+            public E next() {
+                E e = elements[cursor];
+                cursor++;
+                return e;
+            }
+        };
+        return it;
+    }
+
+    /**
+     * Devuelve el elemento en la posición especificada en la lista.
+     *
+     * @param index la posición del elemento que se quiere obtener
+     * @return el elemento en la posición index
+     * @throws IndexOutOfBoundsException si el índice está fuera del rango de la lista
+     */
+    @Override
+    public E get(int index) {
+        if (index < 1 || index > effectiveSize) {
+            throw new IndexOutOfBoundsException("Índice fuera de rango: " + index);
+        }
+        return elements[index - 1];
+    }
+
+    /**
+     * Reemplaza el elemento en la posición especificada en la lista con el nuevo elemento
+     *
+     * @param index la posición del elemento que se va a reemplazar
+     * @param element el nuevo elemento que va a reemplazar al anterior en su misma posición
+     * @return el elemento que estaba antes de que se isnerte el nuevo
+     * @throws IndexOutOfBoundsException si el índice está fuera del rango de la lista
+     */
+    @Override
+    public E set(int index, E element) {
+        if (index < 1 || index > effectiveSize) {
+            throw new IndexOutOfBoundsException("No se pudo establecer el objeto en la posición: " + index);
+        }
+        E previousElement = elements[index - 1];
+        elements[index - 1] = element;
+        return previousElement;
+    }
+
+    /**
+     * Elimina el elemento en la posición especificada en la lista.
+     *
+     * @param index la posición del elemento que se va a eliminar
+     * @return el elemento eliminado
+     * @throws IndexOutOfBoundsException si el índice está fuera del rango o la lista está vacía
+     */
+    @Override
+    public E remove(int index) {
+        if (index < 1 || index > effectiveSize || isEmpty()) {
+            throw new IndexOutOfBoundsException("No se pudo establecer el objeto en la posición: " + index);
+        }
+        E removedElement = elements[index-1];
+        for(int i=index-1; i<effectiveSize;i++){
+            elements[i]=elements[i+1];
+        }
+        elements[effectiveSize-1]=null;
+        effectiveSize--;
+        return removedElement;
+    }
+
 
     @Override
     public E removeFirst() {
@@ -74,90 +235,14 @@ public class ArrayList<E> implements List<E>{
     }
 
     @Override
-    public int size() {
-        return effectiveSize;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return effectiveSize == 0;
-    }
-
-    @Override
     public void clear() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
     public List<E> findIntersection(List<E> otherList){
-      throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    @Override
-    public void add(int index, E element) {
-        if (index < 0 || index > effectiveSize) {
-            // throw new IndexOutOfBoundsException("Invalid index: " + index);
-            System.out.println("No se pudo anadir.");
-        }
-        if (isFull()) {
-            addCapacity();
-        }
-        for (int i = effectiveSize; i > index; i--) {
-            elements[i] = elements[i - 1];
-            // elements[i+1] = elements[i]; MAL
-        }
-        elements[index] = element;
-        effectiveSize++;
-        
-    }
 
-    @Override
-    public E remove(int index) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public E get(int index) {
-        //comienza a contar desde el 1
-        return elements[index-1];
-    }
-
-    @Override
-    public E set(int index, E element) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    private void addCapacity() {
-        E[] tmp = (E[]) new Object[capacity * 2];
-        for (int i = 0; i < capacity; i++){
-            tmp[i] = elements[i];
-        }
-        elements = tmp;
-        capacity = capacity * 2;
-    }
     
-    public String toString() {
-        String s="";
-        for (int i=0; i<effectiveSize; i++) {
-            s+=elements[i]+", ";
-        }
-        return s;
-    }
-    
-    public Iterator<E> iterator(){
-            Iterator<E> it=new Iterator<E>() {
-                int cursor = 0;
-                @Override
-                public boolean hasNext() {
-                    return cursor < effectiveSize;
-                }
-
-                @Override
-                public E next() {
-                    E e=elements[cursor];
-                    cursor++;
-                    return e;
-                }
-            };
-        return it;   
-    }
 }
