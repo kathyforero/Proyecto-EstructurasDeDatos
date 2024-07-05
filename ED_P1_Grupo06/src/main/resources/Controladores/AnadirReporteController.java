4b8eef80996230a735e94f9e54caddd2a65fbc6a
Katherine Forero <143484307+kathyforero@users.noreply.github.com>
2024-07-05 00:45:06 -0500

    Vista y Creación de Reportes + Controladores

---
+++ ED_P1_Grupo06/src/main/resources/Controladores/AnadirReporteController.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+/**
+ * FXML Controller class
+ *
+ * @author Kathy
+ */
+public class AnadirReporteController implements Initializable {
+
+
+    @FXML
+    private AnchorPane txtDesc;
+    @FXML
+    private Label lblUser;
+    @FXML
+    private Button btnAgregar;
+    @FXML
+    private TableView<?> tvCatDesc;
+    @FXML
+    private ComboBox<?> cbCategoria;
+    @FXML
+    private Button btnEliminar;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void subirImagen(ActionEvent event) {
+    }
+
+}
---
+++ ED_P1_Grupo06/src/main/resources/Controladores/VistaReporteController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+/**
+ * FXML Controller class
+ *
+ * @author Kathy
+ */
+public class VistaReporteController implements Initializable {
+
+
+    @FXML
+    private AnchorPane txtDesc;
+    @FXML
+    private Label lblUser;
+    @FXML
+    private TableView<String> tvCatDesc;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
